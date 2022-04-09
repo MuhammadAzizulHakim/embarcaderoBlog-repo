a1b23c9c2a212c7c92266b17837dc47c37e31682
Muhammad Azizul Hakim <19azizul92@gmail.com>
2022-04-10 06:28:36 +0700

    Update scikitlearnApp.py

--- Article20 - ScikitLearn4D Demo/scikitlearnApp.py
+++ Article20 - ScikitLearn4D Demo/scikitlearnApp.py
@@ -2,6 +2,7 @@
 from numpy import where
 from sklearn.datasets import make_classification
 import matplotlib.pyplot as plt
+matplotlib.use("Agg")
 
 # Define dataset
 X, y = make_classification(n_samples=1000, n_features=2, n_informative=2, n_redundant=0, n_clusters_per_class=1, random_state=4)
