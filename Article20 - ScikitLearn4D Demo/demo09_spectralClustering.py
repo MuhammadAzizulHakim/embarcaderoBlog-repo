9afb7f32cccfe902c75cbe4cf0a2bccfc954dfd6
Muhammad Azizul Hakim <19azizul92@gmail.com>
2022-04-25 06:09:00 +0700

    Update demo09_spectralClustering.py

--- Article20 - ScikitLearn4D Demo/demo09_spectralClustering.py
+++ Article20 - ScikitLearn4D Demo/demo09_spectralClustering.py
@@ -4,6 +4,7 @@ from numpy import where
 from sklearn.datasets import make_classification
 from sklearn.cluster import SpectralClustering
 import matplotlib.pyplot as plt
+import matplotlib
 matplotlib.use("Agg")
 
 # Define dataset
