d55fd529f5106bd7a13e883925af44c502531326
Muhammad Azizul Hakim <19azizul92@gmail.com>
2022-04-25 04:36:33 +0700

    Update demo06_miniBatch_KMeans.py

--- Article20 - ScikitLearn4D Demo/demo06_miniBatch_KMeans.py
+++ Article20 - ScikitLearn4D Demo/demo06_miniBatch_KMeans.py
@@ -18,9 +18,9 @@ clusters = unique(yhat)
 
 # Create scatter plot for samples from each cluster
 for cluster in clusters:
-	# Get row indexes for samples with this cluster
-	row_ix = where(yhat == cluster)
-	# Create scatter of these samples
-	plt.scatter(X[row_ix, 0], X[row_ix, 1])
+    # Get row indexes for samples with this cluster
+    row_ix = where(yhat == cluster)
+    # Create scatter of these samples
+    plt.scatter(X[row_ix, 0], X[row_ix, 1])
 # Show the plot
 plt.savefig("scikitlearnImage.jpg")
