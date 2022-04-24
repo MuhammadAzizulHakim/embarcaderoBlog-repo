2cc3bb6016f6bfbcd27d73cf8bab9cf31703d4d3
Muhammad Azizul Hakim <19azizul92@gmail.com>
2022-04-25 03:44:22 +0700

    Update demo02_agglomerativeClustering.py

--- Article20 - ScikitLearn4D Demo/demo02_agglomerativeClustering.py
+++ Article20 - ScikitLearn4D Demo/demo02_agglomerativeClustering.py
@@ -1,24 +1,24 @@
-# agglomerative clustering
+# Agglomerative clustering
 from numpy import unique
 from numpy import where
 from sklearn.datasets import make_classification
 from sklearn.cluster import AgglomerativeClustering
 import matplotlib.pyplot as plt
 
-# define dataset
+# Define dataset
 X, _ = make_classification(n_samples=1000, n_features=2, n_informative=2, n_redundant=0, n_clusters_per_class=1, random_state=4)
-# define the model
+# Define the model
 model = AgglomerativeClustering(n_clusters=2)
-# fit model and predict clusters
+# Fit model and predict clusters
 yhat = model.fit_predict(X)
-# retrieve unique clusters
+# Retrieve unique clusters
 clusters = unique(yhat)
 
-# create scatter plot for samples from each cluster
+# Create scatter plot for samples from each cluster
 for cluster in clusters:
-    # get row indexes for samples with this cluster
+    # Get row indexes for samples with this cluster
     row_ix = where(yhat == cluster)
-    # create scatter of these samples
+    # Create scatter of these samples
     plt.scatter(X[row_ix, 0], X[row_ix, 1])
-# show the plot
+# Show the plot
 plt.savefig("scikitlearnImage.jpg")
