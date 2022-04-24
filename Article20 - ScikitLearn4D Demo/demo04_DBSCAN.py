647f4eaf25381274cb820ce5be938b383634ab23
Muhammad Azizul Hakim <19azizul92@gmail.com>
2022-04-25 04:34:30 +0700

    Update demo04_DBSCAN.py

--- Article20 - ScikitLearn4D Demo/demo04_DBSCAN.py
+++ Article20 - ScikitLearn4D Demo/demo04_DBSCAN.py
@@ -15,9 +15,9 @@ yhat = model.fit_predict(X)
 clusters = unique(yhat)
 # Create scatter plot for samples from each cluster
 for cluster in clusters:
-	# Get row indexes for samples with this cluster
-	row_ix = where(yhat == cluster)
-	# Create scatter of these samples
-	plt.scatter(X[row_ix, 0], X[row_ix, 1])
+    # Get row indexes for samples with this cluster
+    row_ix = where(yhat == cluster)
+    # Create scatter of these samples
+    plt.scatter(X[row_ix, 0], X[row_ix, 1])
 # Show the plot
 plt.savefig("scikitlearnImage.jpg")
