81deae0068c8914b733dc1cd9bf42ab3e71345eb
Muhammad Azizul Hakim <19azizul92@gmail.com>
2022-04-25 05:17:39 +0700

    Update demo08_OPTICS.py

--- Article20 - ScikitLearn4D Demo/demo08_OPTICS.py
+++ Article20 - ScikitLearn4D Demo/demo08_OPTICS.py
@@ -4,6 +4,7 @@ from numpy import where
 from sklearn.datasets import make_classification
 from sklearn.cluster import OPTICS
 import matplotlib.pyplot as plt
+matplotlib.use("Agg")
 
 # Define dataset
 X, _ = make_classification(n_samples=1000, n_features=2, n_informative=2, n_redundant=0, n_clusters_per_class=1, random_state=4)
